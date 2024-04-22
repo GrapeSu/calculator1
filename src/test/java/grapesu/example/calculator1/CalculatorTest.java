5873aac28e8d0cc633d0a0a4f3b8f7fc51a06e25
wangsj <wangsj@office.rtplumbing.com>
2024-04-22 09:09:32 +0800

    init use spring boot context

--- src/test/java/grapesu/example/calculator1/CalculatorTest.java
+++ src/test/java/grapesu/example/calculator1/CalculatorTest.java
@@ -1,11 +1,15 @@
 package grapesu.example.calculator1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class CalculatorTest {
-    private Calculator calculator = new Calculator();
+    @Autowired
+    private Calculator calculator ;
     @Test
     public void testSum() {
         assertEquals(5, calculator.sum(2, 3));
