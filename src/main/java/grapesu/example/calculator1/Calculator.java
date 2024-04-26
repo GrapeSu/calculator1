1920518d5089408f7da2321772fe666ab1a92026
wangsj <wangsj@office.rtplumbing.com>
2024-04-26 16:51:16 +0800

    init use spring boot context

--- src/main/java/grapesu/example/calculator1/Calculator.java
+++ src/main/java/grapesu/example/calculator1/Calculator.java
@@ -1,14 +1,28 @@
 package grapesu.example.calculator1;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
+    @Cacheable("sum")
     public int sum(int a,int b) {
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return a+b;
     }
-
+    @Cacheable("subtract")
     public int subtract(int a,int b) {
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return a-b;
     }
 }
--- src/main/java/grapesu/example/calculator1/Calculator1Application.java
+++ src/main/java/grapesu/example/calculator1/Calculator1Application.java
@@ -1,13 +1,22 @@
 package grapesu.example.calculator1;
 
+import com.hazelcast.client.config.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class Calculator1Application {
 	private static final String CONSTANT = "constant";
 	public static void main(String[] args) {
 		SpringApplication.run(Calculator1Application.class, args);
 	}
-
+	@Bean
+	public ClientConfig hazelcastClientConfig() {
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.getNetworkConfig().addAddress("49.234.184.13");
+		return clientConfig;
+	}
 }
